0c1134046892a9e98a4270437f98ee47cbf5741b
Irina <irinagmarques@hotmail.com>
2021-10-04 14:13:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# React App - GraphQL data from Apollo Client
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExchangeRates from "./components/ExchangeRates";
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Fist React App with GraphQL and Apollo Client</h1>
+      <ExchangeRates />
+    </div>
+  );
+}
+
+export default App;
